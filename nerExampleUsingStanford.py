2aa65b8e09f05ea6c886cb70f12a1bc2af156550
vivekanandpoojari <vivekanand.poojari@gmail.com>
2018-04-08 18:13:42 +0530

    Add files via upload
    NER using stanford

---
+++ nerExampleUsingStanford.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from nltk.tag.stanford import CoreNLPNERTagger
+
+'''
+Start the stanford server uisng the command java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
+This command should be called from the directory where the stanford nlp is downloaded
+The default port picked by server is 9000
+'''
+
+nerTagger = CoreNLPNERTagger(url='http://localhost:9000')
+inputText = 'Your order  has been shipped from Mumbai and will reach Pune in 3 days'
+entities = nerTagger.tag(inputText.split())
+
+for entity in entities:
+	if entity[1] == 'CITY':
+		print('The city is : ' + entity[0])
+	if entity[1] == 'DURATION':
+		print('The duration is : ' + entity[0])
+	if entity[1] == 'ORGANIZATION':
+		print('The organization is : ' + entity[0])
