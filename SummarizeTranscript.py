9af281a63271b648b2fc2f6317bb9f4ea0b69779
Vivekanand Poojari <vpoojaridooj@avaya.com>
2018-05-01 23:45:44 +0530

    Tensorflow impl

--- SummarizeTranscript.py
+++ SummarizeTranscript.py
@@ -14,12 +14,18 @@ import pickle
 from nltk.corpus import stopwords
 import numpy as np
 from sklearn import linear_model
+import tensorflow as tf
 
 ####### Global Variable ########################
 r = Rake()
 nerTagger = CoreNLPNERTagger(url='http://localhost:9000')
 stanfordQuestionClassifier = StanfordCoreNLP('http://localhost:9000')
 stop_words = set(stopwords.words('english'))
+baseDirLocation = '/Users/csdkpune/Documents/temp/Hackathon_2018'
+
+######################################################
+def getFilePath(fileName):
+    return baseDirLocation+"/"+fileName
 
 ######################################################
 
@@ -118,7 +124,7 @@ def eval(allTheLines):
     x_test = getInputFeatureVector(allTheLines)
     y = ''
 
-    with open("/Users/Sarnava/Data/hackathon/Hackathon_2018/logisticRegressionModel.sav", 'rb') as logisticRegressionModel:
+    with open(getFilePath("logisticRegressionModel.sav"), 'rb') as logisticRegressionModel:
         model = pickle.load(logisticRegressionModel)
         y = model.predict(x_test)
 
@@ -151,20 +157,109 @@ def printOutputFile(lines):
 
 ######################################################
 
+def trainTensorflow(allInputLines):
+    # number of features
+    num_features = 4
+    # number of target labels
+    num_classes = 2
+    # learning rate (alpha)
+    learning_rate = 0.05
+    # batch size
+    batch_size = 15
+    # number of epochs
+    num_steps = 50
+
+    X = tf.placeholder("float", [None, num_features], name="xInput")
+    # same with labels: number of classes is known, while number of instances is left undefined
+    Y = tf.placeholder("float",[None, num_classes], name="yInput")
+
+    y_test = [[0, 1], [0, 1], [0, 1], [1, 0], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [1, 0], [0, 1], [1, 0], [1, 0], [0, 1]]
+
+    # W - weights array
+    W = tf.Variable(tf.truncated_normal([num_features, num_classes]))
+    # B - bias array
+    B = tf.Variable(tf.zeros([num_classes]))
+
+    # Define a model
+    # a simple linear model y=wx+b wrapped into softmax
+    pY = tf.nn.softmax(tf.matmul(X, W) + B, name='outputOperation')
+    # pY will contain predictions the model makes, while Y contains real data
+
+    # Define a cost function
+    cost_fn = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pY, labels=Y))
+    # You could also put it in a more explicit way
+    # cost_fn = -tf.reduce_sum(Y * tf.log(pY))
+
+    # Define an optimizer
+    # I prefer Adam
+    opt = tf.train.AdamOptimizer(0.01).minimize(cost_fn)
+    # but there is also a plain old SGD if you'd like
+    #opt = tf.train.GradientDescentOptimizer(0.01).minimize(cost_fn)
+
+    # Create and initialize a session
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver(max_to_keep=1)
+
+    num_epochs = 500
+    for i in range(num_epochs):
+        # run an optimization step with all train data
+        sess.run(opt, feed_dict={X:getInputFeatureVector(allInputLines), Y:y_test})
+        print(i)
+        # thus, a symbolic variable X gets data from train_X, while Y gets data from train_Y
+        # Now assess the model
+        # create a variable which reflects how good your predictions are
+        # here we just compare if the predicted label and the real label are the same
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pY,1), tf.argmax(Y,1)), "float"))
+    # and finally, run calculations with all test data
+    accuracy_value = sess.run(accuracy, feed_dict={X:getInputFeatureVector(allInputLines), Y:y_test})
+    print(accuracy_value)
+    saver.save(sess, getFilePath('my_test_model'), global_step=num_epochs)
+
+######################################################
+
+def evalTensorflow(allInputLines):
+    summaryLines = list()
+    with tf.Session() as sess1:    
+        saver = tf.train.import_meta_graph(getFilePath('my_test_model-500.meta'))
+        saver.restore(sess1, tf.train.latest_checkpoint(baseDirLocation))
+        graph = tf.get_default_graph()
+        a = tf.get_default_graph().get_tensor_by_name('outputOperation:0')
+        x_input = tf.get_default_graph().get_tensor_by_name('xInput:0')
+        v = sess1.run(a, feed_dict={x_input:getInputFeatureVector(allInputLines)})
+        print(v)
+        ouputTensorflow = tf.argmax(v, 1).eval()
+
+    index = 0
+    for inputLine in allInputLines:
+        if (ouputTensorflow[index] == 0):
+            statementlist = inputLine.split(':')
+            statement = statementlist[-1].strip()
+            print(statement)
+            summaryLines.append(statement)
+        index+=1
+    return summaryLines    
+
+######################################################
+
 def main():
     global outputFilenNo
 
-    fileno = "/Users/Sarnava/Data/hackathon/Hackathon_2018/transcript"
+    fileno = getFilePath("transcript")
     summaryLines = list()
     with  open(str(fileno)+".txt", "r") as inputFileHandler:
         lines = inputFileHandler.readlines()
-        #train(lines)
-        summaryLines = eval(lines)
+        #trainTensorflow(lines)
+        summaryLines = evalTensorflow(lines)
+        print(summaryLines)
+        #summaryLines = eval(lines)
+        #print(summaryLines)
         printOutputFile(summaryLines)
-
+        
     with open(str(fileno)+"_ouptput.txt", "w+") as outputFileHandler:
         for summaryLine in summaryLines:
             outputFileHandler.write(summaryLine+"\n")   
+            
 
 ##############################################################        
 if __name__ == "__main__": main()
