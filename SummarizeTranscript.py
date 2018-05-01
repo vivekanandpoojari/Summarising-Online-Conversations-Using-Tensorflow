d254982ad7f7445c02377554ad73c3b5ad386e98
Vivekanand Poojari <vpoojaridooj@avaya.com>
2018-05-01 15:56:36 +0530

    Changed to use logistic regression over the extracted features. The lhs rhs logic is now not required, we use extraction

--- SummarizeTranscript.py
+++ SummarizeTranscript.py
@@ -4,315 +4,167 @@ import inspect
 import nltk
 from os import listdir
 from nltk import word_tokenize, pos_tag
-from nltk.corpus import nps_chat
-from time import time,sleep
 from rake_nltk import Rake
 from collections import Counter
 from nltk.tag.stanford import CoreNLPNERTagger
 from tkinter import *
 from pycorenlp import StanfordCoreNLP
 from nltk.tree import Tree
-import csv
 import pickle
+from nltk.corpus import stopwords
+import numpy as np
+from sklearn import linear_model
 
 ####### Global Variable ########################
-outputFilenNo = 0
-typeofdialogue =[]
-classifier_statement = classifier_template = 0
-RHSClassifierModelName = '/Users/Sarnava/Data/hackathon/Hackathon_2018/rhsClassifierModel.sav'
-NPSDialogActClassifierModelName = '/Users/Sarnava/Data/hackathon/Hackathon_2018/NPSDialogActClassifierModel.sav'
-posts = nps_chat.xml_posts()[:]
 r = Rake()
 nerTagger = CoreNLPNERTagger(url='http://localhost:9000')
 stanfordQuestionClassifier = StanfordCoreNLP('http://localhost:9000')
-domainKeyWords = []
-lineScores = list()
-keywordsToAbstractDictionary = {}
-
+stop_words = set(stopwords.words('english'))
 
 ######################################################
 
-def dialogue_act_features(post):
-     features = {}
-     for word in word_tokenize(post):
-         features['contains({})'.format(word.lower())] = True
-     return features
-
-######################################################
-
-def dialogue_act_features2(inputText):
-    features = {}
+def getCountOfKeywords(inputText):
     r.extract_keywords_from_text(inputText)
     inputTrainingSentenceKeyWords = ' '.join(r.get_ranked_phrases())
+    countOfKeywords = 0
 
     for word in word_tokenize(inputTrainingSentenceKeyWords):
-         features['{}'.format(word.lower())] = True
-    return features
+        if word not in stop_words:
+         countOfKeywords += 1
+    return countOfKeywords
 
 ######################################################
 
-def dialogue_act_features3(inputText):
-    features = {}
-    features['{}'.format(inputText.lower())] = True
-    return features         
-
-######################################################
-
-def train_npschat():
-    global classifier_statement
-    featuresets = []
-    for post in posts:
-        featuresets += [(dialogue_act_features2(post.text), post.get('class'))]
-    train_set = featuresets[:]
-    classifier_statement = nltk.NaiveBayesClassifier.train(train_set)
-    pickle.dump(classifier_statement, open(NPSDialogActClassifierModelName, 'wb'))
-
-#############################################
-    
-def train_template():
-    global classifier_template
-    global domainKeyWords
-    featuresets = []
-    with open('keywordsToAbstractMapping.csv') as keywordsToAbstractMappingFile:
-        keywordsToAbstractMap = csv.reader(keywordsToAbstractMappingFile, delimiter=',')
-        for keywordsToAbstractEntry in keywordsToAbstractMap:
-            for keyword in keywordsToAbstractEntry[0].split(';'):
-                featuresets += [(dialogue_act_features3(keyword), keywordsToAbstractEntry[1])]
-                domainKeyWords += keyword
-    train_set = featuresets[:]
-    #print(train_set)
-    classifier_template = nltk.NaiveBayesClassifier.train(train_set)
-    pickle.dump(classifier_template, open(RHSClassifierModelName, 'wb'))
-
-#############################################
-    
-def populate_domain_keywords():
-    global domainKeyWords
-    global keywordsToAbstractDictionary
-
-    with open('keywordsToAbstractMapping.csv') as keywordsToAbstractMappingFile:
-        keywordsToAbstractMap = csv.reader(keywordsToAbstractMappingFile, delimiter=',')
-        for keywordsToAbstractEntry in keywordsToAbstractMap:
-            keywordsToAbstractDictionary[keywordsToAbstractEntry[0]] = keywordsToAbstractEntry[1]
-            for keyword in keywordsToAbstractEntry[0].split(';'):
-                #print(keyword)
-                domainKeyWords.append(keyword)
-    print(keywordsToAbstractDictionary)            
-
-#############################################
-
-def abstruct(allTheLines):
-    global lineScores
-
-    outputFileHandler = open(str(outputFilenNo)+"_ouptput.txt", "w+")
-
-    outputFileHandler.write("\n=====================================\n")       
-    outputFileHandler.write("Abstract \n")   
-    outputFileHandler.write("=======================================\n")    
-
-    lastSpeaker = lastStatementType = lastLine = ""
+def getInputFeatureVector(inputLines):
+    lineScores = list()
+    lastStatementType = ""
+    lastStatement = ""
+    isAnswerToServiceRequired = False
     index = 0
 
-    for eachLine in allTheLines:
-        index += 1
+    for eachLine in inputLines:
+        isAnswerToServiceRequired = False
         bFoundMatchingEntity = False
         statementlist = eachLine.split(':')
         statement = statementlist[-1].strip()
+
+        index+=1
+
+        if (lastStatementType == 'whQuestion') and lastStatement.find('help') != -1 :
+            isAnswerToServiceRequired = True
 
         # classify the statement as a whQuestion or ynQuestion
         questionClassificationOutput = stanfordQuestionClassifier.annotate(statement, properties={'annotators': 'parse, sentiment','outputFormat': 'json'})
         questionClassificationTree = Tree.fromstring(questionClassificationOutput['sentences'][0]['parse'])
         questionClassificationTreeProductions = questionClassificationTree.productions()
-        standfordQuestionClassification = ''
         
+        classifiedType = ''    
 
         for questionClassificationTreeLevel in questionClassificationTreeProductions:
             if (str(questionClassificationTreeLevel.lhs()) == 'ROOT' and str(questionClassificationTreeLevel.rhs()[0]) == 'SBARQ'):
-                standfordQuestionClassification = 'whQuestion'
-            elif (str(questionClassificationTreeLevel.lhs()) == 'ROOT' and str(questionClassificationTreeLevel.rhs()[0]) == 'SQ'):
-                standfordQuestionClassification = 'ynQuestion'
+                classifiedType = 'whQuestion'
+                break
         
-        classifiedType = classifier_statement.classify(dialogue_act_features2(statement))
-        
-        # Overwrite question classification using stanford since its more reliable than nps chat corpus
-        if standfordQuestionClassification == 'whQuestion' or standfordQuestionClassification == 'ynQuestion':
-            classifiedType = standfordQuestionClassification
-        
-        print(statement + " ||  " + classifiedType)
-        
-        if classifiedType not in typeofdialogue:
-            typeofdialogue.append(classifiedType)
-        
-        if classifiedType == "":
-            continue
-        elif classifiedType == "Greet" or classifiedType == "System":
-            continue
-        elif classifiedType == "ynQuestion" or classifiedType == 'whQuestion':
-            classified_statement = mapInputTextToLHS(statement)   
+        countOfDomainKeywords = getCountOfKeywords(statement)    
 
-            if classifier_statement != '': 
-                outputFileHandler.write(classified_statement+" : ")
-                print('Input = ' + statement + ' , classified using templates as : ' + classified_statement)
-                print('I found a LHS which is : ' + classified_statement)
+        #Check for any entities to print     
+        entities = nerTagger.tag(statement.split())
 
-            '''
-            print(dialogue_act_features2(statement))
-            classified_statement = classifier_template.classify(dialogue_act_features2(statement))
-            '''
-        elif classifiedType == 'Statement' or classifiedType == 'Clarify' or classifiedType == 'Other':
-            if lastStatementType == 'whQuestion':
-                outputFileHandler.write(statement+"\n")
-                print('I found a RHS which is : ' + statement)
-            else:    
-                #Check for any entities to print     
-                entities = nerTagger.tag(statement.split())
+        for entity in entities:
+            if entity[1] != 'O':
+                countOfDomainKeywords += 1            
 
-                for entity in entities:
-                    if entity[1] != 'O':
-                        bFoundMatchingEntity = True
-    
-                if bFoundMatchingEntity:
-                    outputFileHandler.write("Notes : " + statement)
-                    outputFileHandler.write("\n")
-                    print('I found a note which is : ' + statement)
-
-                for entity in entities:
-                    if entity[1] != 'O':
-                        #outputFileHandler.write(entity[1] + " : " + entity[0] + "\n")
-                        print('I found a RHS which is : ' + entity[0])
-                        bFoundMatchingEntity = True
-            
-        elif classifiedType == 'yAnswer' and lastStatementType == 'ynQuestion':
-            print('Found yes answer to ynQuestion')
-            outputFileHandler.write('Yes'+"\n")
-            print('I found a RHS which is : ' + statement)
-        elif classifiedType == 'nAnswer' and lastStatementType == 'ynQuestion':
-            print('Found no answer to ynQuestion')
-            outputFileHandler.write('No'+"\n")
-            print('I found a RHS which is : ' + statement)
-
-        countOfDomainKeywords = 0    
-        for domainKeyword in domainKeyWords:
-            if statement.find(domainKeyword) == -1:
-                continue
-            else:
-                countOfDomainKeywords += 1
-
-        if bFoundMatchingEntity:
-            countOfDomainKeywords += 1        
-
-        lineScore = (statement, len(statement), questionClassificationOutput['sentences'][0]['sentimentValue'], countOfDomainKeywords, index)
+        lineScore = (len(statement), questionClassificationOutput['sentences'][0]['sentimentValue'], countOfDomainKeywords, int(isAnswerToServiceRequired))
         lineScores.append(lineScore)
-            
-        lastSpeaker = statementlist[0]
         lastStatementType = classifiedType
-        lastLine = statementlist[-1]
+        lastStatement = statement
 
-    lineLengthArray = list(zip(*lineScores))[1]
-    sentimentArray = [ int(lineScore[2]) for lineScore in lineScores ]
-    keywordCountArray = list(zip(*lineScores))[3]
+    lineLengthArray = list(zip(*lineScores))[0]
+    sentimentArray = [ int(lineScore[1]) for lineScore in lineScores ]
+    keywordCountArray = list(zip(*lineScores))[2]
 
-    #lineScore = (statement, len(statement), questionClassificationOutput['sentences'][0]['sentimentValue'], countOfDomainKeywords, 0)   
-    print(lineScores)
-
-    lineScores2 = list()
+    x_test = list()
+    
     for lineScore in lineScores:
-        lineLengthNormalized = (lineScore[1] - min(lineLengthArray)) / (max(lineLengthArray) - min(lineLengthArray))
-        sentimentNormalized = (int(lineScore[2]) - min(sentimentArray)) / (max(sentimentArray) - min(sentimentArray))
-        keywordCountNormalized = (lineScore[3] - min(keywordCountArray)) / (max(keywordCountArray) - min(keywordCountArray))
-        lineScore2 = (lineScore[0], round((0.70 * lineLengthNormalized + 0.15 * sentimentNormalized + 0.15 * keywordCountNormalized), 2), lineScore[4])
-        lineScores2.append(lineScore2)
+        lineLengthNormalized = round((lineScore[0] - min(lineLengthArray)) / (max(lineLengthArray) - min(lineLengthArray)), 3)
 
-    outputFileHandler.write("\n=====================================\n")       
-    outputFileHandler.write("Summary\n")   
-    outputFileHandler.write("========================================\n")    
+        if (max(sentimentArray) - min(sentimentArray)) == 0:
+            sentimentNormalized = 0
+        else:          
+            sentimentNormalized = round((int(lineScore[1]) - min(sentimentArray)) / (max(sentimentArray) - min(sentimentArray)), 3)
 
-    a = sorted(lineScores2, key=lambda x: x[1], reverse=True)    
+        if (max(keywordCountArray) -  min(keywordCountArray) == 0):
+            keywordCountNormalized = 0
+        else:
+            keywordCountNormalized = round((lineScore[2] - min(keywordCountArray)) / (max(keywordCountArray) - min(keywordCountArray)), 3)
 
-    b = sorted(a[:2], key=lambda x: x[2])    
+        x_test.append([lineLengthNormalized, sentimentNormalized, keywordCountNormalized, lineScore[3]])
 
-    for lineScore2 in b:
-        outputFileHandler.write(str(lineScore2[0]) + "\n")
+    return x_test    
 
-    outputFileHandler.close()
+######################################################
+
+def train(allTheLines):    
+    x_test = getInputFeatureVector(allTheLines)
+    y_test = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0]
+
+    logreg = linear_model.LogisticRegression(class_weight='balanced')
+    logreg.fit(x_test, y_test)
+    pickle.dump(logreg, open("/Users/Sarnava/Data/hackathon/Hackathon_2018/logisticRegressionModel.sav", 'wb'))
         
-
-######################################################
-
-def printOutputFile():
-     
-     root = Tk()
-     
-     printFileHandler = open(str(outputFilenNo)+"_ouptput.txt", "r")
-     
-     lines = printFileHandler.readlines()
-     heightOfBox = len(lines) + 20
-     T = Text(root, height=heightOfBox, width=100)
-     T.pack()
-     
-     for line in lines:
-          T.insert(END, line)
-          T.insert(END, "\n")
-     b2 = Button(root, text='Quit', command=root.quit)
-     b2.pack(side=RIGHT, padx=15, pady=15)
-     mainloop()
-     
-
-######################################################
-               
-def mapInputTextToLHS(inputText):
-    global keywordsToAbstractDictionary
-    countOfMatchingKeyWords = 0
-    maxKeywordsFound = 0
-    LHS = ''
-    for key, value in keywordsToAbstractDictionary.items() :
-        countOfMatchingKeyWords = 0
-        for keyword in key.split(";"):
-            if inputText.find(keyword) != -1:
-                countOfMatchingKeyWords += 1
-                print('found matching keyword ' + keyword + ' , countOfMatchingKeyWords = ' + str(countOfMatchingKeyWords))
-
-        if countOfMatchingKeyWords > maxKeywordsFound:
-            maxKeywordsFound = countOfMatchingKeyWords
-            LHS = value        
-            print('found matching keyword ' + key + ' , countOfMatchingKeyWords = ' + str(countOfMatchingKeyWords) + ' , value = ' + value)
-    return LHS                 
-
 ######################################################
                 
+def eval(allTheLines):    
+    x_test = getInputFeatureVector(allTheLines)
+    y = ''
+
+    with open("/Users/Sarnava/Data/hackathon/Hackathon_2018/logisticRegressionModel.sav", 'rb') as logisticRegressionModel:
+        model = pickle.load(logisticRegressionModel)
+        y = model.predict(x_test)
+
+    index = 0
+    summaryLines = list()
+    for inputLine in allTheLines:
+        if (y[index] == 1):
+            statementlist = inputLine.split(':')
+            statement = statementlist[-1].strip()
+            summaryLines.append(statement)
+        index+=1
+    return summaryLines    
+        
+######################################################
+
+def printOutputFile(lines):     
+    root = Tk()
+     
+    #printFileHandler = open(str(outputFileName)+"_ouptput.txt", "r")
+    heightOfBox = len(lines) + 20
+    T = Text(root, height=heightOfBox, width=100)
+    T.pack()
+     
+    for line in lines:
+        T.insert(END, line)
+        T.insert(END, "\n")
+    b2 = Button(root, text='Quit', command=root.quit)
+    b2.pack(side=RIGHT, padx=15, pady=15)
+    mainloop()
+
+######################################################
+
 def main():
     global outputFilenNo
-    global classifier_statement
-    global classifier_template
-    global lineScores
 
     fileno = "/Users/Sarnava/Data/hackathon/Hackathon_2018/transcript"
+    summaryLines = list()
+    with  open(str(fileno)+".txt", "r") as inputFileHandler:
+        lines = inputFileHandler.readlines()
+        #train(lines)
+        summaryLines = eval(lines)
+        printOutputFile(summaryLines)
 
-    mode = 'eval'    
+    with open(str(fileno)+"_ouptput.txt", "w+") as outputFileHandler:
+        for summaryLine in summaryLines:
+            outputFileHandler.write(summaryLine+"\n")   
 
-    populate_domain_keywords()
-    #print(domainKeyWords)
-    
-    if mode == 'train':
-        train_npschat()
-        train_template()
-    elif mode == 'eval':
-        with open(RHSClassifierModelName, 'rb') as f:
-            classifier_template = pickle.load(f)
-        with open(NPSDialogActClassifierModelName, 'rb') as g:
-            classifier_statement = pickle.load(g)
-    
-    filesToProcess = [1]
-    outputFilenNo = fileno
-    ans = ""
-    inputFileHandler = open(str(fileno)+".txt", "r")
-    lines = inputFileHandler.readlines()
-    abstruct(lines)
-    inputFileHandler.close()
-    printOutputFile()    
-
-
-    
-        
 ##############################################################        
 if __name__ == "__main__": main()
